514a5fd9dc124be29c3249013d526331e5cea089
akashpatel <akashpatel7204@gmail.com>
2018-12-29 15:10:19 +0530

    theme added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,65 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('pages.index');
+    })->name('admin.index');
+    
+    Route::get('/tables', function () {
+        return view('pages.tables');
+    })->name('admin.tables');
+    
+    Route::get('/flot', function () {
+        return view('pages.flot');
+    })->name('admin.flot');
+
+    Route::get('/morris', function () {
+        return view('pages.morris');
+    })->name('admin.morris');
+
+     Route::get('/forms', function () {
+        return view('pages.forms');
+    })->name('admin.forms');
+
+
+    Route::get('/panels-wells', function () {
+        return view('pages.panels-wells');
+    })->name('admin.panels-wells');
+
+    Route::get('/buttons', function () {
+        return view('pages.buttons');
+    })->name('admin.buttons');
+
+    Route::get('/notifications', function () {
+        return view('pages.notifications');
+    })->name('admin.notifications');
+
+    Route::get('/typography', function () {
+        return view('pages.typography');
+    })->name('admin.typography');
+
+    Route::get('/icons', function () {
+        return view('pages.icons');
+    })->name('admin.icons');
+
+    Route::get('/grid', function () {
+        return view('pages.grid');
+    })->name('admin.grid');
+
+    Route::get('/blank', function () {
+        return view('pages.blank');
+    })->name('admin.blank');
+
+    Route::get('/login', function () {
+        return view('pages.login');
+    })->name('admin.login');
+
+
+    
+
+    
+
+});
